2e59d7727e9ffcd5411fb88de5685a007d3c8651
Rahul5215 <127139130+Rahul5215@users.noreply.github.com>
2026-02-09 17:36:10 +0530

    Add files via upload

---
+++ Scripts/gold/8. Gold Layer - Procedure.sql
@@ -0,0 +1,197 @@
+CREATE OR REPLACE PROCEDURE gold.load_gold()
+LANGUAGE plpgsql
+AS
+$$
+DECLARE
+start_time TIMESTAMP;
+end_time TIMESTAMP;
+duration INTERVAL;
+t_start_time TIMESTAMP;
+t_end_time TIMESTAMP;
+t_duration INTERVAL;
+BEGIN
+start_time := clock_timestamp();
+RAISE NOTICE '============================================';
+RAISE NOTICE 'LOADING GOLD LAYER';
+RAISE NOTICE '============================================';
+
+
+------------------------------------------------------
+-- DIMENTION TABLE : dim_customers
+------------------------------------------------------
+t_start_time := clock_timestamp();
+RAISE NOTICE '--------------------------------------------';
+RAISE NOTICE 'LOADING TABLE : gold.dim_customers ';
+RAISE NOTICE '--------------------------------------------';
+
+RAISE NOTICE 'TRUNCATING TABLE : gold.dim_customers';
+TRUNCATE TABLE gold.dim_customers;
+
+RAISE NOTICE 'INSERTING DATA INTO : gold.dim_customers';
+INSERT INTO gold.dim_customers
+SELECT
+customer_id,
+name,
+email,
+phone,
+city,
+signup_date
+FROM silver.customers;
+
+t_end_time := clock_timestamp();
+t_duration := t_end_time - t_start_time;
+RAISE NOTICE 'LOADING TIME FOR TABLE gold.dim_customers: %',t_duration;
+
+
+
+------------------------------------------------------
+-- DIMENTION TABLE : dim_products
+------------------------------------------------------
+t_start_time := clock_timestamp();
+RAISE NOTICE '--------------------------------------------';
+RAISE NOTICE 'LOADING TABLE : gold.dim_products ';
+RAISE NOTICE '--------------------------------------------';
+
+RAISE NOTICE 'TRUNCATING TABLE : gold.dim_products';
+TRUNCATE TABLE gold.dim_products;
+
+RAISE NOTICE 'INSERTING DATA INTO : gold.dim_products';
+INSERT INTO gold.dim_products
+SELECT
+DISTINCT
+product_id
+FROM silver.order_items;
+
+t_end_time := clock_timestamp();
+t_duration := t_end_time - t_start_time;
+RAISE NOTICE 'LOADING TIME FOR TABLE gold.dim_products: %',t_duration;
+
+
+
+------------------------------------------------------
+-- DIMENTION TABLE : dim_dates
+------------------------------------------------------
+t_start_time := clock_timestamp();
+RAISE NOTICE '--------------------------------------------';
+RAISE NOTICE 'LOADING TABLE : gold.dim_dates ';
+RAISE NOTICE '--------------------------------------------';
+
+RAISE NOTICE 'TRUNCATING TABLE : gold.dim_dates';
+TRUNCATE TABLE gold.dim_dates;
+
+RAISE NOTICE 'INSERTING DATA INTO : gold.dim_dates';
+INSERT INTO gold.dim_dates
+SELECT DISTINCT
+  order_ts::DATE AS date,
+  EXTRACT(YEAR FROM order_ts) AS year,
+  EXTRACT(MONTH FROM order_ts) AS month,
+  EXTRACT(DAY FROM order_ts) AS day,
+  TO_CHAR(order_ts, 'Day') AS weekday
+FROM silver.orders;
+
+t_end_time := clock_timestamp();
+t_duration := t_end_time - t_start_time;
+RAISE NOTICE 'LOADING TIME FOR TABLE gold.dim_dates: %',t_duration;
+
+
+------------------------------------------------------
+-- FACT TABLE : fact_orders
+------------------------------------------------------
+t_start_time := clock_timestamp();
+RAISE NOTICE '--------------------------------------------';
+RAISE NOTICE 'LOADING TABLE : gold.fact_orders ';
+RAISE NOTICE '--------------------------------------------';
+
+RAISE NOTICE 'TRUNCATING TABLE : gold.fact_orders';
+TRUNCATE TABLE gold.fact_orders;
+
+RAISE NOTICE 'INSERTING DATA INTO : gold.fact_orders';
+INSERT INTO gold.fact_orders
+SELECT
+o.order_id,
+o.order_ts,
+o.customer_id,
+o.order_status,
+o.order_amount,
+o.currency,
+o.source_system,
+s.shipment_id,
+s.carrier,
+s.shipped_date,
+s.is_shipped,
+s.delivered_date,
+s.is_delivered
+FROM silver.orders o
+LEFT JOIN silver.shipments s
+on o.order_id = s.order_id;
+
+t_end_time := clock_timestamp();
+t_duration := t_end_time - t_start_time;
+RAISE NOTICE 'LOADING TIME FOR TABLE gold.fact_orders: %',t_duration;
+
+
+------------------------------------------------------
+-- FACT TABLE : fact_order_items
+------------------------------------------------------
+t_start_time := clock_timestamp();
+RAISE NOTICE '--------------------------------------------';
+RAISE NOTICE 'LOADING TABLE : gold.fact_order_items ';
+RAISE NOTICE '--------------------------------------------';
+
+RAISE NOTICE 'TRUNCATING TABLE : gold.fact_order_items';
+TRUNCATE TABLE gold.fact_order_items;
+
+RAISE NOTICE 'INSERTING DATA INTO : gold.fact_order_items';
+INSERT INTO gold.fact_order_items
+SELECT
+order_id,
+product_id,
+quantity,
+unit_price,
+(quantity*unit_price) AS revenue
+FROM silver.order_items;
+
+t_end_time := clock_timestamp();
+t_duration := t_end_time - t_start_time;
+RAISE NOTICE 'LOADING TIME FOR TABLE gold.fact_order_items: %',t_duration;
+
+
+------------------------------------------------------
+-- FACT TABLE : fact_order_items
+------------------------------------------------------
+t_start_time := clock_timestamp();
+RAISE NOTICE '--------------------------------------------';
+RAISE NOTICE 'LOADING TABLE : gold.fact_payments ';
+RAISE NOTICE '--------------------------------------------';
+
+RAISE NOTICE 'TRUNCATING TABLE : gold.fact_payments';
+TRUNCATE TABLE gold.fact_payments;
+
+RAISE NOTICE 'INSERTING DATA INTO : gold.fact_payments';
+INSERT INTO gold.fact_payments
+SELECT
+payment_id,
+order_id,
+payment_amount,
+payment_status,
+payment_ts AS payment_times
+FROM silver.payments;
+
+t_end_time := clock_timestamp();
+t_duration := t_end_time - t_start_time;
+RAISE NOTICE 'LOADING TIME FOR TABLE gold.fact_payments: %',t_duration;
+
+
+
+end_time := clock_timestamp();
+duration := end_time - start_time;
+RAISE NOTICE '============================================';
+RAISE NOTICE 'TOTAL LOADING TIME : %', duration;
+RAISE NOTICE '============================================';
+END
+$$
+
+
+call gold.load_gold()
+
+
---
+++ Scripts/gold/Analysis.sql
@@ -0,0 +1,305 @@
+select
+*
+from gold.fact_orders
+
+select
+*
+from gold.fact_order_items
+
+select
+*
+from gold.dim_customers
+
+select
+*
+from gold.dim_products
+
+--Total Orders Per Day
+select
+extract(day from order_ts),
+count(order_id) 
+from gold.fact_orders
+group by extract(day from order_ts)
+order by extract(day from order_ts)
+
+--Daily revenue trend
+select
+order_ts::date,
+sum(order_amount) as daily_revenue
+from gold.fact_orders
+group by order_ts::date
+order by order_ts::date asc
+
+--Orders by city
+select
+c.city,
+count(o.order_id) as total_orders
+from gold.fact_orders o
+left join gold.dim_customers c
+on c.customer_id = o.customer_id
+group by c.city
+
+--delivery performance by carrier
+select
+distinct
+carrier,
+count(order_id) over(partition by carrier)
+from gold.fact_orders
+where is_delivered is true
+
+--Payment success rate
+select
+payment_status,
+count(*)
+from gold.fact_payments
+group by payment_status
+
+--top five products by revenue
+with cte as(
+select
+product_id,
+sum(revenue) as total_revenue
+from gold.fact_order_items
+group by product_id
+)
+,cte2 as
+(
+select
+*,
+dense_rank() over(order by total_revenue desc) as ranking
+from cte
+)
+select
+*
+from cte2 
+where ranking < 6
+
+--average order value
+select
+avg(order_amount) as avg_order_value
+from gold.fact_orders
+
+--revenue by month
+select
+extract(month from order_ts) as months,
+sum(order_amount) as total_revenue
+from gold.fact_orders
+group by extract(month from order_ts)
+order by extract(month from order_ts) 
+
+--orders vs payment missmatch
+select
+o.order_id
+from gold.fact_orders o
+left join gold.fact_payments p
+on o.order_id = p.order_id
+and p.payment_status = 'SUCCESS'
+where p.order_id is null
+
+--Orders without shipment
+SELECT o.order_id
+FROM gold.fact_orders o
+LEFT JOIN silver.shipments s
+ON o.order_id = s.order_id
+WHERE s.order_id IS NULL;
+
+select * from gold.fact_orders
+
+select
+order_id
+from gold.fact_orders
+where is_shipped is false
+
+
+--CLV
+select
+c.customer_id,
+c.city,
+count(distinct o.order_id) as order_count,
+sum(order_amount) as total_revnue,
+avg(order_amount) as avg_order_value
+from gold.fact_orders o
+join gold.dim_customers c
+on o.customer_id = c.customer_id
+group by c.customer_id,c.city
+order by sum(order_amount) desc
+
+--revenue contribution per product
+with cte as(
+select
+product_id,
+sum(revenue) as revenue
+from gold.fact_order_items
+group by product_id
+)
+,cte2 as(
+select
+*,
+sum(revenue) over() as total_revenue
+from cte
+)
+select
+*,
+concat(round((revenue * 100 / total_revenue),2),'%') as contribution
+from cte2
+
+
+--first order vs repeat order analysis
+WITH first_order AS (
+  SELECT 
+    customer_id,
+    MIN(order_ts) AS first_order_ts
+  FROM gold.fact_orders
+  GROUP BY customer_id
+)
+SELECT 
+  f.customer_id,
+  COUNT(CASE WHEN o.order_ts = f.first_order_ts THEN 1 END) AS first_orders,
+  COUNT(CASE WHEN o.order_ts > f.first_order_ts THEN 1 END) AS repeat_orders
+FROM gold.fact_orders o
+JOIN first_order f
+ON o.customer_id = f.customer_id
+GROUP BY f.customer_id;
+
+
+--Top 3 products per months.
+with cte as(
+select
+extract(month from o.order_ts) as months,
+oi.product_id,
+sum(oi.revenue) as total_sales
+from gold.fact_orders o
+join gold.fact_order_items oi
+on oi.order_id = o.order_id
+group by extract(month from o.order_ts),oi.product_id
+)
+,cte2 as(
+select 
+*,
+dense_rank() over(partition by months order by total_sales desc) as ranking
+from cte
+)
+select
+*
+from cte2
+where ranking < 4
+
+--Payment failure rate by day
+SELECT 
+  payment_times,
+  COUNT(*) AS total_payments,
+  SUM(CASE WHEN payment_status = 'FAILED' THEN 1 ELSE 0 END) AS failed_payments,
+  ROUND(100.0 *
+     SUM(CASE WHEN payment_status = 'FAILED' THEN 1 ELSE 0 END)
+     / COUNT(*), 2) AS failure_rate_pct
+FROM gold.fact_payments
+GROUP BY payment_times
+ORDER BY payment_times;
+
+
+--Time gap between order and delivery
+select
+order_id,
+shipped_date,
+delivered_date,
+(delivered_date - shipped_date) as delivery_days
+from gold.fact_orders
+where is_delivered is true
+
+--Worst performing carrier
+with cte as(
+select 
+carrier,
+count(order_id) as total_orders,
+sum(case when is_delivered then 1 else 0 end) as delivered_orders
+from gold.fact_orders
+group by carrier
+)
+select
+*,
+concat((delivered_orders * 100 / total_orders),'%') as deilbery_success_rate
+from cte
+
+
+--Top three performing products of each month per year.
+with cte as(
+select
+extract(year from o.order_ts) as years,
+extract(month from o.order_ts) as months,
+oi.product_id,
+p.product_name,
+sum(oi.revenue) as total_revenue
+from gold.fact_order_items oi
+left join gold.fact_orders o
+on o.order_id = oi.order_id
+left join gold.dim_products p
+on oi.product_id = p.product_id
+group by extract(year from o.order_ts), extract(month from o.order_ts), oi.product_id, p.product_name
+)
+, cte2 as(
+select 
+*,
+dense_rank() over(partition by years, months order by total_revenue desc) as ranking
+from cte
+)
+select
+*
+from cte2
+where ranking <= 3
+
+--Orders by day-of-week
+select
+to_char(order_ts, 'day') as weekdays,
+count(order_id) as total_orders,
+sum(order_amount) as total_revenue
+from gold.fact_orders
+group by to_char(order_ts, 'day')
+order by to_char(order_ts, 'day')
+
+--Revenue by city + month
+select
+extract(month from o.order_ts) as months,
+c.city,
+sum(order_amount) as total_revenue
+from gold.fact_orders o
+left join gold.dim_customers c
+on o.customer_id = c.customer_id
+group by extract(month from o.order_ts), c.city
+
+--Orders per customer bucket
+with cte as(
+select 
+customer_id,
+count(order_id) as orders
+from gold.fact_orders
+group by customer_id
+order by customer_id
+)
+,cte2 as(
+select
+*,
+case when orders > 10 then 'Loyal Customers'
+     when orders < 2  then 'One Time Buyer'
+	 else 'Regular Customers'
+	 end as customer_segment
+from cte
+)
+select
+customer_segment,
+count(*) as number_of_customers
+from cte2
+group by customer_segment
+
+
+
+
+
+
+call bronze.load_bronze()
+call silver.load_silver()
+call gold.load_gold()
+
+
+select *
+from gold.fact_orders
+
